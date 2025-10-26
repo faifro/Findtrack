3cf90804715f9f6fa262a6db355df2b6597e7370
faifro <francograff@hotmail.com>
2025-10-26 20:29:09 -0300

    Allow configuring port via environment

--- app.py
+++ app.py
@@ -536,4 +536,5 @@ def projections_delete(pid):
     return redirect(url_for('projections_list'))
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=8080, debug=True)
+    port = int(os.environ.get('PORT', 8080))
+    app.run(host='0.0.0.0', port=port, debug=True)
